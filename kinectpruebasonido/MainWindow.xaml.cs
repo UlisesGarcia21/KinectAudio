df74cd4ca9030cb85bb9652a1a51229faa79d183
Uli21 <nalau_tkd@live.com.mx>
2016-08-06 00:21:55 -0500

    Cambiar a inicio con kinectSensor; agregar identificador de dirección de fuente de sonido

--- kinectpruebasonido/MainWindow.xaml.cs
+++ kinectpruebasonido/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Resources;
 using System.Windows.Shapes;
 
 namespace kinectpruebasonido
@@ -27,36 +29,106 @@ namespace kinectpruebasonido
     public partial class MainWindow : Window
     {   
         //Campos de clase principal
-        KinectSensor sensorKinect;  //Campo que hace referencia a clase KinectSensor de Microsoft.Kinect
+        KinectSensor sensorActivo;   //Campo que es instancia de clase KinectSensor
+        private const int intervaloPoleoAudio = 50; //Campo que define tiempo en ms entre cada lectura de audio
+        private const int muestraPorMilisegundo = 16;   //Campo que define cantidad de muestras obtenidas cada milisegundo
+        private const int bytesPorMuestra = 2;  //Campo que define número de bytes en cada muestra de audio
+        private readonly byte[] bufferAudio = new byte[intervaloPoleoAudio * muestraPorMilisegundo * bytesPorMuestra];  //Campo de buffer para almacenar datos de registro de audio
+
+        private readonly double[] energia = new double[(uint)(780*1.25)];   //Campo para buffer que almacena datos de energía conforme se lee el audio
+        private readonly object bloqueoEnergia = new object();  //Campo que hace referencia a un objeto que asegura buffer de energía para sincronización de canales
+        
         private Stream registroAudio; //Campo que hace referencia a clase Stream de System.IO
         private bool lectura;   //Campo que permite activar o desactivar la captura de sonido
-        private Thread RamaLectura;     //Campo que hace referencia a conducto independiente de adquisición de sonido
+        private Thread canalLectura;     //Campo que hace referencia a conducto independiente de adquisición de sonido
 
         public MainWindow() //Constructor que inicia aplicación WPF
         {
             InitializeComponent();  //Iniciar componentes de ventana
-            Loaded += IniciarKinect;    //Evento que inicia sensor Kinect
         }
-        
-        private void IniciarKinect(object sender, EventArgs e) //Método privado, estático y sin retorno que inicia sensor Kinect
+
+        private void IniciarKinect(object sender, RoutedEventArgs e) //Método privado, estático y sin retorno que inicia sensor Kinect
         {
-            foreach (var sensorpotencial in KinectSensor.KinectSensors) //Buscar en colección de KinectSensors
+            foreach (var sensorPotencial in KinectSensor.KinectSensors)
             {
-                if (sensorpotencial.Status == KinectStatus.Connected)   //Si estado de sensor es conectado
+                if(sensorPotencial.Status == KinectStatus.Connected)
                 {
-                    this.sensorKinect = sensorpotencial;    //Variable sensorpotencial será asignada a campo sensorKinect
-                    break;                                  //Interrumpir búsqueda
+                    this.sensorActivo = sensorPotencial;
+                    break;
                 }
             }
-            if (this.sensorKinect != null)  //Si se encontró un sensor no nulo
+
+            if(null != this.sensorActivo)
             {
-                this.sensorKinect.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);   //Registro de color en formato RGB, resolución 640x480 píxeles y muestreo de 30 Hz
-                this.sensorKinect.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);      //Registro de profundidad con resolución 640x480 píxeles y muestreo de 30 Hz
-                this.sensorKinect.SkeletonStream.Enable();  //Activar rastreo de esqueleto con propiedades de suavizado por defecto
-                this.sensorKinect.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);  //Registro de infrarrojo con resolución 640x480 píxeles y muestreo de 30 Hz
-                this.sensorKinect.Start();    //Inicia sensor Kinect 
+                try
+                {
+                    this.sensorActivo.Start();
+                }
+                catch(IOException)
+                {
+                    this.sensorActivo = null;
+                }
+            }
+            if(null == this.sensorActivo)
+            {
+                MessageBox.Show("Sensor desconectado");
+            }
+                        
+                this.sensorActivo.AudioSource.BeamAngleChanged += this.CampoAudioModificado; //Controlador de evento BeamAngleChanged
+                this.sensorActivo.AudioSource.SoundSourceAngleChanged += this.FuenteAudioModificada;  //Controlador de evento SoundSourceAngleChanged
+                this.registroAudio = this.sensorActivo.AudioSource.Start();  //Iniciar registro de audio
+
+                //Iniciar canal separado para adquisición de audio
+                this.lectura = true;    //Activar registro de audio
+                this.canalLectura = new Thread(CanalLecturaAudio);  //Crear nuevo canal para registro de audio
+                this.canalLectura.Start();  //Iniciar registro de audio
+        }
+
+        private void DetenerRegistroAudio(object sender, CancelEventArgs e) //Método para finalizar adquisición de audio
+        {
+            this.lectura = false;   //Desactivar registro de audio
+            if (null != canalLectura)
+            {
+                canalLectura.Join();    //Detener ejecución de canal creado previamente
+            }
+            if (null != this.sensorActivo)
+            {
+                this.sensorActivo.AudioSource.BeamAngleChanged -= this.CampoAudioModificado; //Desenlazar controlador de evento BeamAngleChanged
+                this.sensorActivo.AudioSource.SoundSourceAngleChanged -= this.FuenteAudioModificada; //Desenlazar controlador de evento SoundSourceAngleChanged
+                this.sensorActivo.AudioSource.Stop();    //Detener registro de audio
+                this.sensorActivo.Stop();    //Inhabilitar sensor detectado
+                this.sensorActivo = null;
             }
         }
+
+        private void CampoAudioModificado(object sender, BeamAngleChangedEventArgs e)   //Método que actúa como controlador de evento BeamAngleChanged
+        {
+            rotacionCono.Angle = -e.Angle;  //Mostrar dirección de campo de audio cada vez que se modifica
+        }
+
+        private void FuenteAudioModificada(object sender, SoundSourceAngleChangedEventArgs e)   //Método que actúa como controlador de evento SoundSourceAngleChanged
+        {
+            const double anchoMinimoGradiente = 0.04;   //Máxima confianza con este ancho de gradiente
+            double mitadAncho = Math.Max((1 - e.ConfidenceLevel), anchoMinimoGradiente) / 2;
+            this.sourceGsPre.Offset = Math.Max(this.sourceGsMain.Offset - mitadAncho, 0);
+            this.sourceGsPost.Offset = Math.Min(this.sourceGsMain.Offset+mitadAncho,1);
+            fuenteRotacion.Angle = -e.Angle;
+        }
         
+        private void CanalLecturaAudio()    //Método que controla canal independiente para registro de sonidos
+        {
+            while(this.lectura) //Mientras el campo lectura sea verdadero
+            {
+                int conteoLectura = registroAudio.Read(bufferAudio,0,bufferAudio.Length);
+                lock(this.bloqueoEnergia)
+                {
+                    for (int i = 0; i < conteoLectura; i += 2)
+                    {
+                        short muestraAudio = BitConverter.ToInt16(bufferAudio, i);
+                    }
+                }
+            }
+        }
+
     }
 }
